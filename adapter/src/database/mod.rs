458172fd88332c59fd6d4426b00f26c83ab1dc39
Yuri Takahashi <ty_tsuruya@icloud.com>
2024-10-26 14:30:35 +0900

    chapter5.2

--- adapter/src/database/mod.rs
+++ adapter/src/database/mod.rs
@@ -1,5 +1,6 @@
 use shared::config::DatabaseConfig;
 use sqlx::{postgres::PgConnectOptions, PgPool};
+pub mod model;
 
 fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
     PgConnectOptions::new()
@@ -14,6 +15,9 @@ fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
 pub struct ConnectionPool(PgPool);
 
 impl ConnectionPool {
+    pub fn new(pool: PgPool) -> Self {
+        Self(pool)
+    }
     pub fn inner_ref(&self) -> &PgPool {
         &self.0
     }
